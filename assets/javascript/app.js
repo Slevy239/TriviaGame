f7c4f54cd892dfe3ad2636bce43603c7ab34d997
Sam Levy <slevy239@gmail.com>
2019-09-06 16:24:48 -0400

    correction

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-setTimeout(fiveSeconds, 1000 * 5);
+setTimeout(fiveSeconds, 1000 * 2);
 
 
 
 function fiveSeconds() {
-    $("#time-left").append("<h2>Peen</h2>");
+    $("#time-left").append("<h2>Timer</h2>");
     console.log("10 seconds left");
   }
   
