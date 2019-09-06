a30d69562e53b370fa742c344ac76e1a6db76c53
Sam Levy <slevy239@gmail.com>
2019-09-06 17:51:46 -0400

    button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,25 @@
 $(document).ready(function () {
 
-    var questions = ["why?"]
+    var questions = ["why?", ]
+    var poss = ["why not?", "ok", "2", "6"]
     var answers = ["why not?"]
 
 
 
     function timer () {
-        var counter = 5;
+        var counter = 15;
 
         setInterval(function () {
             counter--;
             if (counter >= 0) {
+                for (i = 0; i < poss.length; i++) {
+                var AnswerButton = $("<button>")
+                AnswerButton.text(poss[i]);
                 span = document.getElementById("time-left");
                 span.innerHTML = counter;
                 $("#questions").text(questions);
             }
+        }
             if (counter === 0) {
                 clearInterval(counter);
                 // once counter reaches 0, refresh page and show results
