864de618fe170369dc376a33e3d825b48d2feab0
Sam Levy <slevy239@gmail.com>
2019-09-06 17:05:43 -0400

    timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,27 @@
-setTimeout(fiveSeconds, 1000 * 2);
+$(document).ready(function () {
+
+    var questions = ["why?"]
+    var answers = ["why not?"]
 
 
 
-function fiveSeconds() {
-    $("#time-left").append("<h2>Timer</h2>");
-    console.log("10 seconds left");
-  }
-  
+    function timer () {
+        var counter = 5;
+
+        setInterval(function () {
+            counter--;
+            if (counter >= 0) {
+                span = document.getElementById("time-left");
+                span.innerHTML = counter;
+                $("#questions").text(questions);
+            }
+            if (counter === 0) {
+                clearInterval(counter);
+                // once counter reaches 0, refresh page and show results
+                $("#answers").text(answers);
+            }
+        }, 1000);
+    };
+    timer();
+   
+})
