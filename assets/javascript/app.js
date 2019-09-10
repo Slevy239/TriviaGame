ce8aa43378aba81dc2d4fd7fe643011b2f1753d0
Sam Levy <slevy239@gmail.com>
2019-09-09 22:40:13 -0400

    more functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,19 @@
+  
 $(document).ready(function () {
 
     var quizQuestions = [
         {
-            question: "Why",
-            choices: ["why not", "why not", "why not"],
-            correctAnswer: "Why not"
-        },
-
-        {
             question: "When did Phish start performing as a band?",
             choices: ["1985", "1990", "1983", "1993"],
-            correctAnswer: "1989"
+            correctAnswer: "1983"
 
         },
+        {
+            question: "Why",
+            choices: ["why not", "why not", "why not", "ok"],
+            correctAnswer: "Why not"   
+        },
+
 
         {
             question: "where did Phish orignally form?",
@@ -23,54 +24,105 @@ $(document).ready(function () {
     ]
 
 
-    var count = 30;
+    var counter = 30;
     var currentQuestion = 0;
+    var correct = 0;
+    var incorrect = 0;
     var timer;
+    var correctAnswer;
+
+    function startGame() {
+        $('#start').remove();
+        currentQuestion = 0;
+        correct = 0;
+        incorrect = 0;
+        loadQuestion();
+        timer();
+    }
 
 
     function loadQuestion() {
-        count = 30;
-        timer = setInterval(timer, 1000)
+        answered = false;
+        var correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+        counter = 30;
+        timer = setInterval(timer, 1000);
+        correct = quizQuestions[currentQuestion].correctAnswer;
         var question = quizQuestions[currentQuestion].question;
-        var choices = quizQuestions[currentQuestion].choices;
-
-        $('#questions').html('<h4>' + question + '</h4>');
-        $('#poss').html(choices);
-
-
-        console.log(question)
-        console.log(choices);
-        console.log(quizQuestions.length)
+        $('#questions').html(question);
+        for (var i = 0; i < 4; i++) {
+            var newChoices = quizQuestions[currentQuestion].choices[i];
+            $('#choices').append('<h4 id=' + i + '>' + newChoices + '</h4>')
+        }
+        
+        $("h4").click(function () {
+            var id = $(this).attr('id');
+            if (id = correctAnswer) {
+                answered = true;
+                $('#questions').html('the answer is: ' + quizQuestions[currentQuestion].correctAnswer);
+                correctAnswer();
+                userAnswer();
+            }
+        })
     }
-    loadQuestion();
+    
+    function correctAnswer() {
+        correct++;
+        nextQuestion();
+
+    }
+
+    function incorrectanswer() {
+        incorrect++;
+        nextQuestion()
+
+    }
+
+    function unanswered() {
+        if (counter <= 0) {
+        incorrect++;
+        nextQuestion();
+        }
+    }
+
+    function nextQuestion() {
+        currentQuestion++;
+    }
+
+    function userAnswer() {
+        if (id === correctAnswer) {
+            correctAnswer();
+        } else {
+            incorrectanswer();
+        }
+    }
 
 
-
-    function loadChoices(choices) {
-        var result = '';
-
-        for (let i = 0; i < choices.length; i++) {
-            result += '<p class="choice">'+ choices[i] + '</p>'
+    function results() {
+        if (currentQuestion > 5) {
+            $('#correct').html(correct);
+            $('#correct').html(incorrect);
 
         }
-        return result;
-    }
-    loadChoices();
+    }results();
+
+    $('#start').on("click", function () {
+        $('#start');
+        startGame()
+    })
     
     
-    
-    function timer () {
-            count--;
-            $('#time-left').html(count)
-
-            if (count === 0) {
-                clearTimer();
-            }
-}
-
-
-
-    function clearTimer () {
-        clearInterval(timer)
-    }
-    });
+    function timer() {
+        if (counter === 0) {
+            answered = true;
+            clearInterval(timer);
+            $('#questions').html("correct");
+            unanswered();
+        } else if (answered === true) {
+            clearInterval(timer);
+        } else {
+            counter--;
+            $('#time-left').html(counter);
+        }
+    };
+})
+   
